257e6ba7337e4726f40d28a7615e590f313e3eb3
HieuNguyen9941 <nguyenminhhieu111199@gmail.com>
2020-10-28 19:29:51 +0700

    First commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from .extensions import mongo
+from .TodoRouters import todo
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config.from_object('app.settings')
+app.register_blueprint(todo)
+mongo.init_app(app)
