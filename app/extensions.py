ffc4dfff65678bd685114911e8b974a15e9620aa
HieuNguyen9941 <nguyenminhhieu111199@gmail.com>
2020-10-30 15:28:55 +0700

    hieuthu1

--- app/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_pymongo import pymongo
-from .settings import MONGO_URI
-
-# MONGO_URI='mongodb+srv://Test:12345@cluster0.jrmvt.mongodb.net/todo?retryWrites=true&w=majority'
-
-mongo = pymongo.MongoClient(MONGO_URI)
-
-db = mongo.get_database('todo')
--- readme.md
+++
@@ -1,2 +0,0 @@
-- Khởi tạo môi trường chạy trong cái của anaconda
-> conda create -n kk python=3.7
